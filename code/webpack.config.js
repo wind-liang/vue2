23dc7df5af5185a1b6019c749b78af9eb7d873d3
windliang <648917857@qq.com>
2022-04-05 10:33:52 +0800

    init code

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
-    entry: "./5/data.js",
+    entry: "./5.Vue2剥丝抽茧-响应式系统之深度响应/data.js",
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "bundle.js",
