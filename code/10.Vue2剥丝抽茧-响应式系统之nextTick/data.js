ede919993d19e9028f58969fc1a5f7c853bfd7ec
windliang <648917857@qq.com>
2022-04-13 07:00:20 +0800

    feat: init10

--- code/10.Vue2剥丝抽茧-响应式系统之nextTick/data.js
+++ code/10.Vue2剥丝抽茧-响应式系统之nextTick/data.js
@@ -1,4 +1,3 @@
-// https://vue.windliang.wang/posts/Vue2%E5%89%A5%E4%B8%9D%E6%8A%BD%E8%8C%A7-%E5%93%8D%E5%BA%94%E5%BC%8F%E7%B3%BB%E7%BB%9F%E4%B9%8B%E5%BC%82%E6%AD%A5%E9%98%9F%E5%88%97.html
 import { observe } from "./reactive";
 import Watcher from "./watcher";
 
