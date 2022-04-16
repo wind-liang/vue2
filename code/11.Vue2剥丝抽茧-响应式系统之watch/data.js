b840c5360d8fae6c396aefa3094a4f98933f92fd
windliang <648917857@qq.com>
2022-04-16 11:28:47 +0800

    11init

--- code/11.Vue2剥丝抽茧-响应式系统之watch/data.js
+++ code/11.Vue2剥丝抽茧-响应式系统之watch/data.js
@@ -1,4 +1,3 @@
-// https://vue.windliang.wang/posts/Vue2%E5%89%A5%E4%B8%9D%E6%8A%BD%E8%8C%A7-%E5%93%8D%E5%BA%94%E5%BC%8F%E7%B3%BB%E7%BB%9F%E4%B9%8BnextTick.html
 // document.getElementById("root").innerText = "hello";
 // setTimeout(() => {
 //     document.getElementById("root").innerText = "hello2";
