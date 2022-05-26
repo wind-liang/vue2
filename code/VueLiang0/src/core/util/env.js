1ea32b0c1b877db4743481c9d875e0b3eb11f9d1
windliang <648917857@qq.com>
2022-05-26 08:26:08 +0800

    feat: util分配

--- code/VueLiang0/src/core/util/env.js
+++ code/VueLiang0/src/core/util/env.js
@@ -1,4 +1,7 @@
 /* istanbul ignore next */
 export function isNative(Ctor) {
-  return typeof Ctor === "function" && /native code/.test(Ctor.toString());
+    return typeof Ctor === "function" && /native code/.test(Ctor.toString());
 }
+
+// can we use __proto__?
+export const hasProto = "__proto__" in {};
