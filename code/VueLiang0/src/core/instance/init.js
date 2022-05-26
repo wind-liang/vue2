8b19417fb3db5bff5705d739e888daa1eba2c4cf
windliang <648917857@qq.com>
2022-05-26 08:57:45 +0800

    feat: 删除暂时没有用到的

--- code/VueLiang0/src/core/instance/init.js
+++ code/VueLiang0/src/core/instance/init.js
@@ -1,29 +1,17 @@
 /* @flow */
 
-import { initState } from './state'
-import {mountComponent} from './lifecycle'
-let uid = 0
+import { initState } from "./state";
+import { mountComponent } from "./lifecycle";
 
-export function initMixin (Vue) {
-  Vue.prototype._init = function (options) {
-    const vm = this
-    // a uid
-    vm._uid = uid++
+export function initMixin(Vue) {
+    Vue.prototype._init = function (options) {
+        const vm = this;
+        vm.$options = options;
 
-    // a flag to avoid this being observed
-    vm._isVue = true
+        initState(vm);
 
-    vm.$options = options
+        vm.render = options.render;
 
-
-    // expose real self
-    vm._self = vm
-    initState(vm)
-
-    vm.render = options.render;
-
-    // if (vm.$options.el) {
-      mountComponent(vm)
-    // }
-  }
+        mountComponent(vm);
+    };
 }
