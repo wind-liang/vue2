73e6772bfd2c745a814ac3a7ad7d0d8d4f8a3cb2
windliang <648917857@qq.com>
2022-06-21 06:28:50 +0800

    增删init

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
-    entry: "./19.Vue2剥丝抽茧-虚拟dom之移动优化/main.js",
+    entry: "./20.Vue2剥丝抽茧-虚拟dom之增删/main.js",
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "bundle.js",
